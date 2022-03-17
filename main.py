21404b6da7984392239a866d09ebef3a0ab0fdf2
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-17 13:58:35 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+with open("last.json", "w") as f:
+    data = json.load(f)
+
+base = "https://www3.nhk.or.jp/sokuho/jishin/data"
+
+while True:
+    r = requests.get(base + "/JishinReport.xml")
+    soup = BeautifulSoup(r.text, "html.parser")
+    url = item.get("url")
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    root = soup.find("Root")
+    if data["last"] == root.find("Timestamp").text:
+        image = base + "/" + root.find("Global")
+        requests.post(data["webhook"], json={
+            "embeds": [
+              {
+                  "title": "自身です",
+                  "image": image
+              }
+            ]
+        })
