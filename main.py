a60a96696782f928ddee36e82859cf356ccf423c
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-17 14:00:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-with open("last.json", "w") as f:
+with open("data.json", "r) as f:
     data = json.load(f)
 
 base = "https://www3.nhk.or.jp/sokuho/jishin/data"
@@ -14,7 +14,7 @@ while True:
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     root = soup.find("Root")
-    if data["last"] == root.find("Timestamp").text:
+    if data["last"] != root.find("Timestamp").text:
         image = base + "/" + root.find("Global")
         requests.post(data["webhook"], json={
             "embeds": [
@@ -24,3 +24,6 @@ while True:
               }
             ]
         })
+        data["last"] = root.find("Timestamp").text
+        with open("data.json", "w") as f:
+            json.dump(data, f)
